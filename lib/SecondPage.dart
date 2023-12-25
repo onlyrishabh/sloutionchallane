ff2928fa079c0e47f5b5e8ee26b1c03774a54132
Sahil <116063177+sahil4430@users.noreply.github.com>
2023-12-25 15:39:09 +0530

    Update SecondPage.dart
    color changed

--- lib/SecondPage.dart
+++ lib/SecondPage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:hexcolor/hexcolor.dart';
 
 class secondPage extends StatefulWidget {
   const secondPage({Key? key}) : super(key: key);
@@ -24,10 +25,12 @@ class _secondPageState extends State<secondPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
+     appBar: AppBar(
         leading: BackButton(),
-        backgroundColor: Colors.cyanAccent,
-        title: Text("Contribute",style: TextStyle(fontWeight: FontWeight.bold,fontSize: 20),),
+        backgroundColor: HexColor("89CFF0"),
+        title: Text("Contribute",style: TextStyle(fontWeight: FontWeight.bold,
+            color: HexColor("011A34"),
+            fontSize: 20),),
       ),
 
       body: Padding(
@@ -36,63 +39,68 @@ class _secondPageState extends State<secondPage> {
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             SizedBox(height: 10,),
-            Text("Select Type",style: TextStyle(fontWeight: FontWeight.bold,fontSize: 20)),
+            Text("Select Type",style: TextStyle(fontWeight: FontWeight.bold,color: HexColor("011A34"),
+                fontSize: 20)),
             SizedBox(height: 5,),
             // Padding(
             //   padding: const EdgeInsets.all(8.0),
             //   child:
-              Container(
-                padding: EdgeInsets.only(left: 10,right: 16),
-                decoration: BoxDecoration(
+            Container(
+              padding: EdgeInsets.only(left: 10,right: 16),
+              decoration: BoxDecoration(
                   border: Border.all(color: Colors.grey,width: 1.2),
                   borderRadius: BorderRadius.circular(15)
-                ),
-                child: DropdownButton(
+              ),
+              child: DropdownButton(
                   hint: Text("Select Items: "),
                   dropdownColor: Colors.white,
                   iconSize: 36,
                   isExpanded: true,
                   underline: SizedBox(),
                   style: TextStyle(
-                    color: Colors.black,
-                    fontSize: 22
+                      color: HexColor("011A34"),
+                      fontSize: 22
                   ),
                   value: valueChoose,
-                    items: listitems.map((valueItem){
-                      return DropdownMenuItem(
-                        value: valueItem,
-                        child: Text(valueItem),
-                      );
-                    }).toList(),
-                    onChanged: (newValue){
+                  items: listitems.map((valueItem){
+                    return DropdownMenuItem(
+                      value: valueItem,
+                      child: Text(valueItem),
+                    );
+                  }).toList(),
+                  onChanged: (newValue){
                     setState(() {
                       valueChoose=newValue;
                     });
-                    }
-                ),
+                  }
               ),
+            ),
             // ),
 
             SizedBox(height: 18,),
 
-            Text("Location",style: TextStyle(fontWeight: FontWeight.bold,fontSize: 20)),
+            Text("Location",style: TextStyle(color: HexColor("011A34"),
+                fontWeight: FontWeight.bold,fontSize: 20)),
             SizedBox(height: 5,),
             TextField(
               maxLines: 2,
               decoration: InputDecoration(
                 border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(15)
+                    borderRadius: BorderRadius.circular(15)
                 ),
-                hintText: 'Enter a search term',
-
+                hintText: 'Here will be always the current location \nof the User',hintStyle: TextStyle(
+                color: CupertinoColors.systemGrey,
+                fontSize: 18,
+              )
               ),
             ),
 
             SizedBox(height: 18,),
-            Text("Upload Images",style: TextStyle(fontWeight: FontWeight.bold,fontSize: 20)),
+            Text("Upload Images",style: TextStyle(color: HexColor("011A34"),
+                fontWeight: FontWeight.bold,fontSize: 20)),
             SizedBox(height: 5,),
             Container(
-              child: Icon(Icons.image,size: 80,),
+              child: Icon(Icons.image,color: Colors.grey.withOpacity(0.7),size: 80,),
               height: 100,
               width: MediaQuery.of(context).size.width,
               decoration: BoxDecoration(
@@ -102,7 +110,8 @@ class _secondPageState extends State<secondPage> {
             ),
 
             SizedBox(height: 18,),
-            Text("Details",style: TextStyle(fontWeight: FontWeight.bold,fontSize: 20)),
+            Text("Details",style: TextStyle(color:HexColor("011A34") ,
+                fontWeight: FontWeight.bold,fontSize: 20)),
             SizedBox(height: 5,),
             TextField(
               maxLines: 3,
@@ -121,10 +130,10 @@ class _secondPageState extends State<secondPage> {
               mainAxisAlignment: MainAxisAlignment.spaceAround,
               children: [
                 ElevatedButton(onPressed: (){},
-                    child: Text("Cancel"),
+                  child: Text("Cancel"),
                   style: ElevatedButton.styleFrom(
                     primary: Colors.white,
-                    onPrimary: Colors.black,
+                    onPrimary: HexColor("011A34"),
                     elevation: 2,
                     shape: RoundedRectangleBorder(
                         borderRadius: BorderRadius.circular(15.0)),
@@ -134,9 +143,9 @@ class _secondPageState extends State<secondPage> {
 
                 ElevatedButton(onPressed: (){},
                     style: ElevatedButton.styleFrom(
-                      primary: Colors.black,
+                      primary: HexColor("011A34"),
                       onPrimary: Colors.white,
-                      shadowColor: Colors.cyanAccent,
+                      shadowColor: HexColor("89CFF0"),
                       elevation: 2,
                       shape: RoundedRectangleBorder(
                           borderRadius: BorderRadius.circular(15.0)),
